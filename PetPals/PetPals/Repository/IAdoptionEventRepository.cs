30b933165ee6e242b9236e9a94023b182733e8a9
Gowtham C <146124693+GowthamC06@users.noreply.github.com>
2024-11-22 15:49:26 +0530

    Add files via upload

---
+++ PetPals/PetPals/Repository/IAdoptionEventRepository.cs
@@ -0,0 +1,16 @@
+﻿using PetPals.Model;
+using System;
+using System.Collections.Generic;
+
+namespace PetPals.Repository
+{
+    internal interface IAdoptionEventRepository
+    {
+     
+        List<AdoptionEvent> GetAllAdoptionEvents();
+        AdoptionEvent GetAdoptionEventById(int eventId);
+        void AddAdoptionEvent(AdoptionEvent adoptionEvent);
+        void UpdateAdoptionEvent(AdoptionEvent adoptionEvent);
+        void DeleteAdoptionEvent(int eventId);
+    }
+}
---
+++ PetPals/PetPals/Service/IAdoptionEventService.cs
@@ -0,0 +1,26 @@
+﻿using PetPals.Model;
+using PetPals.Repository;
+using System;
+using System.Collections.Generic;
+
+namespace PetPals.Service
+{
+    internal interface IAdoptionEventService
+    {
+        
+        List<AdoptionEvent> GetAllAdoptionEvents();
+        AdoptionEvent GetAdoptionEventById(int eventId);
+
+     
+        void CreateAdoptionEvent(AdoptionEvent adoptionEvent);
+
+       
+        void UpdateAdoptionEvent(AdoptionEvent adoptionEvent);
+
+        
+        void DeleteAdoptionEvent(int eventId);
+
+       
+        List<AdoptionEvent> GetAdoptionEventsByCriteria(string status, DateTime? startDate, DateTime? endDate);
+    }
+}
